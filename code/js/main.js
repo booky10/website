f7d42c49fad684956f95c5408feb93e340df5ccd
booky10 <booky10@github.com>
2020-10-02 18:40:54 +0200

    added language display

--- code/js/main.js
+++ code/js/main.js
@@ -13,12 +13,23 @@ function refreshLanguageText() {
   $(".lang-de").hide();
   $(".lang-en").hide();
 
-  if (document.cookie.startsWith("lang=de")) $(".lang-de").fadeIn(1000);
-  else $(".lang-en").fadeIn(1000);
+  if (document.cookie.startsWith("lang=de")) {
+    $(".lang-de").fadeIn(1000);
+    updateLanguageText("Deutsch");
+  } else {
+    $(".lang-en").fadeIn(1000);
+    updateLanguageText("English");
+  }
+}
+
+function updateLanguageText(lang) {
+  $("#lang-text").text(lang);
 }
 
 function writeCopyright() {
-  document.write("&copy; " + new Date().getFullYear() + " booky.tk. Made by booky10.");
+  document.write(
+    "&copy; " + new Date().getFullYear() + " booky.tk. Made by booky10."
+  );
 }
 
 function addLines(amount) {
