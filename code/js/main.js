5b373039399baa037a158189a2d8c3dca396f711
booky10 <booky10@github.com>
2020-10-02 15:40:12 +0200

    language basics

--- code/js/main.js
+++ code/js/main.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $(".fade-out").show().fadeOut(1000);
   $(".fade-in").hide().fadeIn(1000);
+  $(".hide").hide();
 });
+
+function setLanguage(language) {
+  document.cookie = "lang=" + language + "; path=/";
+  location.reload();
+}
+
+function refreshLanguageText() {
+  $(".lang-de").hide();
+  $(".lang-en").hide();
+
+  if (document.cookie.startsWith("lang=de")) {
+    $(".lang-de").fadeIn(1000);
+  } else {
+    $(".lang-en").fadeIn(1000);
+  }
+}
