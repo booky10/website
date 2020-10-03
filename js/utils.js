2f976edadef97909013193b61f05cce47a62fc91
booky10 <booky10@github.com>
2020-10-03 15:33:24 +0200

    big update

--- js/utils.js
+++ js/utils.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
   $(".fade-out").show().fadeOut(1000);
   $(".fade-in").hide().fadeIn(1000);
   $(".hide").hide();
-
-  prepareMobile();
 });
 
 function setLanguage(language) {
@@ -16,10 +14,10 @@ function refreshLanguageText() {
   $(".lang-en").hide();
 
   if (document.cookie.startsWith("lang=de")) {
-    $(".lang-de").fadeIn(1000);
+    $(".lang-de").show();
     updateLanguageText("Deutsch");
   } else {
-    $(".lang-en").fadeIn(1000);
+    $(".lang-en").show();
     updateLanguageText("English");
   }
 }
@@ -30,20 +28,10 @@ function updateLanguageText(lang) {
 
 function writeCopyright() {
   document.write(
-    "&copy; " + new Date().getFullYear() + " booky.tk. Made by booky10."
+    "&copy; " + new Date().getFullYear() + " booky.tk, made by booky10."
   );
 }
 
 function addLines(amount) {
   for (let i = 0; i < amount; i++) document.write("<br>");
-}
-
-function prepareMobile() {
-  alert(screen);
-  if (
-    /Mobile|Phone|Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
-      navigator.userAgent
-    )
-  )
-    $(".anti-phone").delay(1100).hide();
 }
