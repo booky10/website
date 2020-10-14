93a9e854294ee556d2ec2f50957a2f13499cd6ba
booky10 <booky10@github.com>
2020-10-14 14:57:24 +0200

    mgscloud-upload.sh update

--- scripts/nextcloud/mgscloud-upload.sh
+++ scripts/nextcloud/mgscloud-upload.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 FOLDER="${1}"
+FILE="${2:-panel.tar.gz}"
+REMOTE="$(/usr/bin/basename ${3:-panel.tar.gz})"
+
+URL='https://mgscloud.net/'
+WEBDAV='public.php/webdav/'
 
 if [ "${FOLDER}" = '' ]; then
   echo 'Please use arguments: <folder> [file]'
   exit
 fi
 
-URL='https://mgscloud.net/'
-WEBDAV='public.php/webdav/'
-
-REMOTE=$(/usr/bin/basename panel.tar.gz)
-FILE="${2:-panel.tar.gz}"
-
 
 echo "Uploading ${FILE}... (Folder: ${FOLDER})"
-curl -T "${FILE}" -u "${FOLDER}":'' -H 'X-Requested-With: XMLHttpRequest' -H 'X-Method-Override: REPLACE' "${URL}${WEBDAV}${REMOTE}"
+curl -T "${FILE}" -u "${FOLDER}":'' -H 'X-Requested-With: XMLHttpRequest' -H 'X-Method-Override: PUT' "${URL}${WEBDAV}${REMOTE}"
