feeb8a9dbc9747781bbfee762895e23ec092f9ba
booky10 <github@booky.tk>
2021-03-12 10:39:01 +0100

    added rick roll warning (totally not a rick roll)

---
+++ rickroll/script.js
@@ -0,0 +1,6 @@
+document.getElementById('back-btn').addEventListener('click', () => gotoRick());
+document.getElementById('continue-btn').addEventListener('click', () => gotoRick());
+
+function gotoRick() {
+  location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
