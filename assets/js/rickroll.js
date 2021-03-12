9215222d7f4b787da9dda7a470bc7bcb358467ca
booky10 <github@booky.tk>
2021-03-12 14:39:18 +0100

    moved the rick roll files around

--- assets/js/rickroll.js
+++ assets/js/rickroll.js
@@ -1,6 +1,4 @@
+const gotoRick = () => location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+
 document.getElementById('back-btn').addEventListener('click', () => gotoRick());
 document.getElementById('continue-btn').addEventListener('click', () => gotoRick());
-
-function gotoRick() {
-  location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-}
