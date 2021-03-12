4967845fe751d14ba94c9418fc7d9f350ebabe1e
booky10 <github@booky.tk>
2021-03-12 14:36:54 +0100

    updated the scripts for pterodactyl

--- scripts/pterodactyl/07-to-10-upgrader.sh
+++ scripts/pterodactyl/07-to-10-upgrader.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-##############################################
-# Just a script to upgrade pterodactyl panel #
-#         version 0.7 to version 1.0         #
-##############################################
-
 echo 'Debug: Checking user'
 if [ "${USER}" != 'root' ]; then
   echo "Error: Please run as root, you are ${USER}!"
-  exit
+  exit 255
 fi
 
 echo 'Info: Setting values'
@@ -19,14 +14,16 @@ PTERODACTYL_DIRECTORY='/var/www/pterodactyl'
 echo 'Info: Verifying installation'
 if [ ! -e "${PTERODACTYL_DIRECTORY}" ]; then
   echo "Error: Pterodactyl not found at ${PTERODACTYL_DIRECTORY}!"
-  exit
+  exit 255
 fi
 
 while true; do
   echo "Info: Do you want to upgrade to panel ${VERSION}?"
   echo 'Warning: DO NOT USE THIS ON CentOS!'
   echo 'Warning: THERE IS NO OFFICIAL WAY OF GOING BACK!'
-  read -p 'Info: [Y(es)/N(o)] ' yn
+
+  read -pr 'Info: [Y(es)/N(o)] ' yn
+
   case $yn in
   [Yy]*) break ;;
   [Nn]*) exit ;;
@@ -35,44 +32,42 @@ while true; do
 done
 
 echo 'Info: Starting installation'
-cd "${PTERODACTYL_DIRECTORY}"
+cd "${PTERODACTYL_DIRECTORY}" || exit 255
 
 echo 'Debug: Turning maintenance mode on'
-php artisan down || exit
-
+php artisan down || exit 255
 
 if [ -e "${PTERODACTYL_DIRECTORY}/panel.tar.gz" ]; then
   echo 'Debug: Old panel.tar.gz found, deleting it'
-  rm -rf "${PTERODACTYL_DIRECTORY}/panel.tar.gz" || exit
+  rm -rf "${PTERODACTYL_DIRECTORY}/panel.tar.gz" || exit 255
 fi
 
 echo 'Debug: Downloading new files'
-curl -L -o 'panel.tar.gz' "https://github.com/pterodactyl/panel/releases/download/${VERSION}/panel.tar.gz" || exit
+curl -L -o 'panel.tar.gz' "https://github.com/pterodactyl/panel/releases/download/${VERSION}/panel.tar.gz" || exit 255
 
 echo 'Debug: Deleting old files'
-rm -rf $(find app public resources -depth | head -n -1 | grep -Fv "$(tar -tf panel.tar.gz)") || exit
+rm -rf "$(find app public resources -depth | head -n -1 | grep -Fv "$(tar -tf panel.tar.gz)")" || exit 255
 
 echo 'Debug: Unpacking new files'
-tar -xzvf 'panel.tar.gz' || exit
-rm -f 'panel.tar.gz' || exit
+tar -xzvf 'panel.tar.gz' || exit 255
+rm -f 'panel.tar.gz' || exit 255
 
 echo 'Debug: Applying permissions'
-chmod -R 755 storage/* bootstrap/cache || exit
+chmod -R 755 storage/* bootstrap/cache || exit 255
 
 echo 'Debug: Updating dependencies with composer'
-composer install --no-dev --optimize-autoloader || exit
+composer install --no-dev --optimize-autoloader || exit 255
 
 echo 'Debug: Clearing caches'
-php artisan view:clear || exit
-php artisan config:clear || exit
+php artisan view:clear || exit 255
+php artisan config:clear || exit 255
 
 echo 'Debug: Updating database'
 php artisan migrate --force
 php artisan db:seed --force
 
 echo 'Debug: Setting permission for www-data'
-chown -R www-data:www-data *
-
+chown -R www-data:www-data -- *
 
 echo 'Debug: Restarting queue worker'
 php artisan queue:restart
@@ -81,5 +76,5 @@ echo 'Debug: Turning maintenance mode off'
 php artisan up
 
 echo "Info: Done upgrading to pterodactyl panel ${VERSION}!"
-cd "${OLD_DIRECTORY}"
+cd "${OLD_DIRECTORY}" || exit 255
 exit
--- scripts/pterodactyl/10-updater-url.sh
+++ scripts/pterodactyl/10-updater-url.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-##############################################
-# Just a script to update pterodactyl panel #
-#           version 1.0 and above           #
-##############################################
-
 echo 'Debug: Checking user'
 if [ "${USER}" != 'root' ]; then
   echo "Error: Please run as root, you are ${USER}!"
-  exit
+  exit 255
 fi
 
 echo 'Info: Setting values'
@@ -19,20 +14,22 @@ PTERODACTYL_DIRECTORY='/var/www/pterodactyl'
 echo 'Info: Checking if url provided'
 if [ "${URL}" = '' ]; then
   echo 'Error: Please provide a url to update to!'
-  exit
+  exit 255
 fi
 
 echo 'Info: Verifying installation'
 if [ ! -e "${PTERODACTYL_DIRECTORY}" ]; then
   echo "Error: Pterodactyl not found at ${PTERODACTYL_DIRECTORY}!"
-  exit
+  exit 255
 fi
 
 while true; do
   echo "Info: Do you want to update to ${URL}?"
   echo 'Warning: DO NOT USE THIS ON CentOS!'
   echo 'Warning: DO NOT USE THIS TO UPGRADE FROM 0.7 TO 1.0!'
-  read -p 'Info: [Y(es)/N(o)] ' yn
+
+  read -rp 'Info: [Y(es)/N(o)] ' yn
+
   case $yn in
   [Yy]*) break ;;
   [Nn]*) exit ;;
@@ -41,39 +38,39 @@ while true; do
 done
 
 echo 'Info: Starting update'
-cd "${PTERODACTYL_DIRECTORY}"
+cd "${PTERODACTYL_DIRECTORY}" || exit 255
 
 echo 'Debug: Turning maintenance mode on'
-php artisan down || exit
+php artisan down || exit 255
 
 if [ -e "${PTERODACTYL_DIRECTORY}/panel.tar.gz" ]; then
   echo 'Debug: Old panel.tar.gz found, deleting it'
-  rm -rf "${PTERODACTYL_DIRECTORY}/panel.tar.gz" || exit
+  rm -rf "${PTERODACTYL_DIRECTORY}/panel.tar.gz" || exit 255
 fi
 
 echo 'Debug: Downloading new files'
-curl -L -o 'panel.tar.gz' "${URL}" || exit
+curl -L -o 'panel.tar.gz' "${URL}" || exit 255
 
 echo 'Debug: Unpacking new files'
-tar -xzvf 'panel.tar.gz' || exit
-rm -f 'panel.tar.gz' || exit
+tar -xzvf 'panel.tar.gz' || exit 255
+rm -f 'panel.tar.gz' || exit 255
 
 echo 'Debug: Applying permissions'
-chmod -R 755 storage/* bootstrap/cache || exit
+chmod -R 755 storage/* bootstrap/cache || exit 255
 
 echo 'Debug: Updating dependencies with composer'
-composer install --no-dev --optimize-autoloader || exit
+composer install --no-dev --optimize-autoloader || exit 255
 
 echo 'Debug: Clearing caches'
-php artisan view:clear || exit
-php artisan config:clear || exit
+php artisan view:clear || exit 255
+php artisan config:clear || exit 255
 
 echo 'Debug: Updating database'
 php artisan migrate --force
 php artisan db:seed --force
 
 echo 'Debug: Setting permission for www-data'
-chown -R www-data:www-data *
+chown -R www-data:www-data -- *
 
 echo 'Debug: Restarting queue worker'
 php artisan queue:restart
@@ -82,5 +79,5 @@ echo 'Debug: Turning maintenance mode off'
 php artisan up
 
 echo "Info: Done upgrading to ${URL}!"
-cd "${OLD_DIRECTORY}"
+cd "${OLD_DIRECTORY}" || exit 255
 exit
