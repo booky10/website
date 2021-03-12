b465cbbb31cff193e7c765b0e928a13bc5ae835d
booky10 <github@booky.tk>
2021-03-12 14:37:09 +0100

    updated the scripts for nextcloud

--- scripts/nextcloud/public-uploader.sh
+++ scripts/nextcloud/public-uploader.sh
@@ -4,22 +4,23 @@ URL="${1}"
 FOLDER="${2}"
 FILE="${3}"
 WEBDAV='public.php/webdav/'
-REMOTE="$(/usr/bin/basename ${4:-${FILE}})"
+REMOTE="$(/usr/bin/basename "${4:-${FILE}}")"
 
 if [ "${FOLDER}" = '' ]; then
   echo 'Please use arguments: <url> <folder> <file> [remote]'
-  exit -1
+  exit 255
 fi
 
 if [ "${URL}" = '' ]; then
   echo 'Please use arguments: <url> <folder> <file> [remote]'
-  exit -1
+  exit 255
 fi
 
 if [ "${FILE}" = '' ]; then
   echo 'Please use arguments: <url> <folder> <file> [remote]'
-  exit -1
+  exit 255
 fi
 
 echo "Uploading ${FILE}..."
-curl -T "${FILE}" -u "${FOLDER}":'' -H 'X-Requested-With: XMLHttpRequest' -H 'X-Method-Override: REPLACE' "${URL}${WEBDAV}${REMOTE}"
+curl -T "${FILE}" -u "${FOLDER}":'' -H 'X-Requested-With: XMLHttpRequest' -H 'X-Method-Override: REPLACE' "${URL}${WEBDAV}${REMOTE}" || exit 255
+exit 0
