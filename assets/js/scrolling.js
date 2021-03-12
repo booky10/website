a7092320464100539271577e7451100a863cf281
booky10 <github@booky.tk>
2021-03-12 14:58:19 +0100

    cleaned up scrolling.js

--- assets/js/scrolling.js
+++ assets/js/scrolling.js
@@ -1,29 +1,16 @@
 (function ($) {
-  console.log('a')
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(() => {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       let target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
       if (target.length) {
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top - 56,
-          },
-          1000,
-          "easeInOutExpo"
-        );
+        $('html, body').animate({scrollTop: target.offset().top - 56}, 1000, 'easeInOutExpo');
         return false;
       }
     }
   });
 
-  $(".js-scroll-trigger").click(function () {
-    $(".navbar-collapse").collapse("hide");
-  });
-
-  $("body").scrollspy({
-    target: "#main-nav",
-    offset: 56,
-  });
+  $('.js-scroll-trigger').click(() => $('.navbar-collapse').collapse('hide'));
+  $('body').scrollspy({target: '#main-nav', offset: 56});
 })(jQuery);
